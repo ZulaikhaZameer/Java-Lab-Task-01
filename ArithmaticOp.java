f1c69e170020b1550ecde425e46fa2614ef17859
ZulaikhaZameer <zulaikhazameer@gmail.com>
2023-11-09 17:54:34 +0500

    Arithmatic operators program

---
+++ ArithmaticOp.java
@@ -0,0 +1,10 @@
+public class ArithmaticOp{
+	public static void main(String ar[]){
+		int n1 = 4;
+		int n2 = 2;
+		System.out.println(n1+ " + "+n2+ " = " + (n1+n2));
+		System.out.println(n1+ " - "+n2+ " = " + (n1-n2));
+		System.out.println(n1+ " x "+n2+ " = " + n1*n2);
+		System.out.println(n1+ " / "+n2+ " = " + n1/n2);
+	}
+}
