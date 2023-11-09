d0fa25a02d4add1185fb048bdf539f65f0d78b1d
ZulaikhaZameer <zulaikhazameer@gmail.com>
2023-11-10 00:30:55 +0500

    Simple printing program

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram{
+	public static void main(String ar[]){
+		System.out.println("Name: Zulaikha");
+		System.out.println("Roll No: 22F-BSCS-43");
+		System.out.println("Date of birth: 03rd March 2004");
+	}
+}
