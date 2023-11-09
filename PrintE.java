73e435627f87584cb162161b25fc9d56c215261a
ZulaikhaZameer <zulaikhazameer@gmail.com>
2023-11-10 00:33:47 +0500

    printed E without loop

---
+++ PrintE.java
@@ -0,0 +1,11 @@
+public class PrintE{
+	public static void main(String ar[]){
+		System.out.println("*****");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*****");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*****");
+	}
+}
