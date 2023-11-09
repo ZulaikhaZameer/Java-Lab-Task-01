e6dba0b8fbf88bbc928219179d83ebfe43660807
ZulaikhaZameer <zulaikhazameer@gmail.com>
2023-11-10 00:32:03 +0500

    Size of data types

---
+++ SizeOf.java
@@ -0,0 +1,10 @@
+public class SizeOf {
+    public static void main(String ar[]){
+        System.out.println("The size of int is: "+ Integer.SIZE / 8 + " bytes");
+        System.out.println("The size of float is: "+ Float.SIZE / 8 + " bytes");
+        System.out.println("The size of double is: "+ Double.SIZE / 8 + " bytes");
+        System.out.println("The size of char is: "+ Character.SIZE / 8 + " bytes");
+    }
+}
+
+
