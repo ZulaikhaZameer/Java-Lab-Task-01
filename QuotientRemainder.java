3930aa0dbec05083da4c324f4ea37a1dcb3bc72c
ZulaikhaZameer <zulaikhazameer@gmail.com>
2023-11-12 19:02:13 +0500

    Quotient and remainder program

---
+++ QuotientRemainder.java
@@ -0,0 +1,10 @@
+public class QuotientRemainder{
+	public static void main(String ar[]){
+		int quotient = 150 / 13;
+		int remainder = 150 % 13;
+		
+		System.out.println("When 150 is divided by 3:");
+		System.out.println("Quotient: "+quotient);
+		System.out.println("Remainder: "+remainder);
+	}
+}
