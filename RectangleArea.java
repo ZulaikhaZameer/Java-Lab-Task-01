243aad4847cc5d3b3b724c2164bdff9a5b200450
ZulaikhaZameer <zulaikhazameer@gmail.com>
2023-11-12 19:03:08 +0500

    Rectangle area program

---
+++ RectangleArea.java
@@ -0,0 +1,17 @@
+public class RectangleArea{
+	public static void main(String ar[]){
+		float width = 3;
+		float length = 5;
+		
+		System.out.println("For rectangle "+width+" inches wides and "+length+" inches long: ");
+		System.out.println("Area : "+ width * length + " sq. inches");
+		System.out.println("Circumference: "+ (2 * (width+length))+ " inches");
+		
+		width = 6.8f;
+		length = 3.3f;
+		
+		System.out.println("For rectangle "+width+" inches wides and "+length+" inches long: ");
+		System.out.println("Area : "+ width * length + " sq. inches");
+		System.out.println("Circumference: "+ (2 * (width+length))+ " inches");
+	}	
+}
